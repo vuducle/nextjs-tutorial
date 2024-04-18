19165d322add11f706394fb288b4ea90248da31a
Vu Minh Le <vuminhle@outlook.com>
2024-04-18 21:19:15 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import '@/app/ui/global.css';
+import { BeVietnamPro } from '@/app/ui/fonts';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${BeVietnamPro.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Be_Vietnam_Pro, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400'],
+});
+
+export const BeVietnamPro = Be_Vietnam_Pro({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+});
